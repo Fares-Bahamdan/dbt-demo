788be5d592c5730f0b092b445c4eec107e8fca7a
FnoizY <fares.bahamdan@gmail.com>
2026-03-11 14:56:24 +0300

    .

--- dbtTesting/models/staging/example_teams_details.sql
+++ dbtTesting/models/staging/example_teams_details.sql
@@ -4,12 +4,15 @@ with teams as (
 
 team_locations as (
     select * from {{ ref("team_locations") }}
-),
+), 
 
 Final as (
     select teams.Teams,
+
     team_locations.city,
+
     team_locations.state,
+    
     teams.teams = '{{ var("current_champion") }}' as is_champion
 
     from teams
